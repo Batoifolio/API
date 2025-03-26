ccdda5c748be3faaa629305c3717b63b8405a7a0
JordiGis <jorgisfer@alu.edu.gva.es>
2025-03-26 22:57:40 +0100

    FIX bind user routes to user controller

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -4,10 +4,10 @@ import { UsersController } from '../controllers/users.controller'
 const UsersRouter = Router()
 const userController = new UsersController()
 
-UsersRouter.get('/', userController.getAllUsers)
-UsersRouter.get('/:id', userController.getUserById)
-// UsersRouter.post('/', userController.getUser)
-// UsersRouter.put('/:id', userController.getUser)
-// UsersRouter.delete('/:id', userController.getUser)
+UsersRouter.get('/', userController.getAllUsers.bind(userController))
+UsersRouter.get('/:id', userController.getUserById.bind(userController))
+// UsersRouter.post('/', userController.getUser.bind(userController))
+// UsersRouter.put('/:id', userController.getUser.bind(userController))
+// UsersRouter.delete('/:id', userController.getUser.bind(userController))
 
 export default UsersRouter
